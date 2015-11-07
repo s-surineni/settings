ad60fe316218e446448c4a0f7b0e8eebcbb6c978
sampath <sampath.surineni@gmail.com>
2015-11-07 14:00:33 +0530

    changed config

--- development.sh
+++ development.sh
@@ -4,15 +4,15 @@ tmux split-window -v -t development
 tmux send-keys -t development:1.1 'sst' C-m
 tmux send-keys -t development:1.2 'mst' C-m
 
-tmux new-window -n client -t development
-tmux send-keys -t development:2 'cs' C-m
-
 tmux new-window -n workers -t development
-tmux split-window -v -t development:3
-tmux split-window -h -t development:3
-tmux send-keys -t development:3.1 'mister' C-m
-tmux send-keys -t development:3.2 'batch' C-m
-tmux send-keys -t development:3.3 'worker' C-m
+tmux split-window -v -t development:2
+tmux split-window -h -t development:2
+tmux send-keys -t development:2.1 'mister' C-m
+tmux send-keys -t development:2.2 'batch' C-m
+tmux send-keys -t development:2.3 'worker' C-m
+
+tmux new-window -n client -t development
+tmux send-keys -t development:3 'cs' C-m
 
 tmux new-window -n console -t development
 tmux select-window -t development:4
