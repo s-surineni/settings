c37da538eb92dcd8ddef7eb6ae33dee87c3f1268
sampath <sampath.skt@gmail.com>
2016-12-04 13:22:54 +0530

    set the current color

--- terminalconfig.sh
+++ terminalconfig.sh
@@ -4,5 +4,6 @@ export HISTFILESIZE=1000000000
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
-
-export PS1="[\w]\$(parse_git_branch)\n\\$ \[$(tput sgr0)\]"
+COLOR1="\[\033[1;33m\]"     #First color
+COLOR2="\[\033[0;33m\]"     #Second color
+export PS1="${COLOR1}[\w]\$(parse_git_branch)\n\\$ \[$(tput sgr0)\]"
