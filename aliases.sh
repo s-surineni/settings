ffa8e4ffd835bc145c02634827418a97b6030c9b
sampath <sampath.skt@gmail.com>
2018-06-09 21:41:20 -0700

    added alias for sudo

--- aliases.sh
+++ aliases.sh
@@ -19,4 +19,4 @@ alias emax="emacsclient -n"                      # used to be "emacs -nw"
 alias semac="sudo emacsclient -t"                # used to be "sudo emacs -nw"
 alias emacsc="emacsclient -c -a emacs"           # new - opens the GUI with alternate non-daemon
 alias sttu="git status -uno"           # new - opens the GUI with alternate non-daemon
-
+alias sudo="sudo "
