8ea4792c22fbe774e70449ffdb8fca5ffef57a82
Sampath Surineni <sampath.skt@gmail.com>
2018-08-19 09:10:05 -0700

    added shortcut for kubectl completion

--- zsh_config.sh
+++ zsh_config.sh
@@ -21,3 +21,5 @@ plugins=(
 ZSH_THEME="bira"
 echo "loading for zshconfig"
 source $ZSH/oh-my-zsh.sh
+
+echo "if [ $commands[kubectl] ]; then source <(kubectl completion zsh); fi" >> ~/.zshrc # add autocomplete permanently to your zsh shell
