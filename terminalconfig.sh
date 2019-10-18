0c82d2d398ce377fff9839e17241fd4edb6ce79b
aviso backup2 <avisobackup2@avisos-MacBook-Pro.local>
2019-10-18 14:55:31 +0530

    made path into a variable

--- terminalconfig.sh
+++ terminalconfig.sh
@@ -1,21 +1,22 @@
+#!/bin/bash
 # place this in .bashrc
 # if [ -f ~/settings/terminalconfig.sh ]; then
 # 	. ~/settings/terminalconfig.sh
 # fi
-
+REPO_LOCATION=~/sampath/settings
 export HISTSIZE=1000000
 export HISTFILESIZE=1000000000
 
 # Source global definitions
-if [ -f ~/settings/aliases.sh ]; then
-	. ~/settings/aliases.sh
+if [ -f $REPO_LOCATION/aliases.sh ]; then
+	. $REPO_LOCATION/aliases.sh
 fi
 
 # Source global definitions
-if [ -f ~/settings/paths.sh ]; then
-	. ~/settings/paths.sh
+if [ -f $REPO_LOCATION/paths.sh ]; then
+	. $REPO_LOCATION/paths.sh
 fi
 
-source ~/settings/zsh_config.sh
+. $REPO_LOCATION/zsh_config.sh
 
-source ~/settings/org_specific.sh
+. $REPO_LOCATION/org_specific.sh
