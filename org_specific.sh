222959700876365f80c51ab7f383356bcecd0ac3
Sampath Surineni <sampath.skt@gmail.com>
2021-01-27 15:18:19 +0530

    commented org specific

--- org_specific.sh
+++ org_specific.sh
@@ -1,6 +1,5 @@
-export JAVA_HOME=~/software/jjava/jdk-12.0.1
-PATH=${JAVA_HOME}/bin:${PATH}
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-export GENV=/home/sampath/projects/aviso/analytics-server/sdkr102
-export GNANA_CLIENT_ENV=/home/sampath/projects/aviso/python-sdk/env
-
+# export JAVA_HOME=~/software/jjava/jdk-12.0.1
+# PATH=${JAVA_HOME}/bin:${PATH}
+# export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+# export GENV=/home/sampath/projects/aviso/analytics-server/sdkr102
+# export GNANA_CLIENT_ENV=/home/sampath/projects/aviso/python-sdk/env
