2f3826ffd620935ff55d45fa9a2f8f8102d213aa
recorder <sampath.surineni@gmail.com>
2023-05-30 07:11:45 +0530

    added spacemacs config

---
+++ my-space-config/key-bindings.el
@@ -0,0 +1,8 @@
+;; movement bindings
+(global-set-key (kbd "M-f") 'forward-char)
+(global-set-key (kbd "M-b") 'backward-char)
+(global-set-key (kbd "M-d") 'delete-char)
+
+(global-set-key (kbd "C-b") 'backward-word)
+(global-set-key (kbd "C-f") 'forward-word)
+(global-set-key (kbd "C-d") 'sp-delete-word)
