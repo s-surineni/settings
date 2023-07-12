729f6e77875669e19cf7bc107761b626ee400dab
sampath <sampath.surineni@gmail.com>
2023-07-12 08:10:20 +0530

    update

--- emacs-profiles.el
+++ emacs-profiles.el
@@ -1,8 +1,13 @@
+;; set user-emacs-directory to where you clone prelude
+;; user-emacs-directory=/home/sampath/projects/eemacs
+;; (setq my-profile
+;;       '("My Profile"
+;;         (user-emacs-directory . "/home/sampath/projects/eemacs")))
 (("default" . ((user-emacs-directory . "~/.emacs.default")))
-("spacemacs" . ((user-emacs-directory . "~/config/spacemacs")
-                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
-;;  ("spacemacs" . ((user-emacs-directory . "~/config/spacemacs")))
- ("prelude" . ((user-emacs-directory . "~/config/prelude")))
+ ;; ("spacemacs" . ((user-emacs-directory . "/home/sampath/projects/eemacs/spacemacs")
+ ;;                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+ ("spacemacs" . ((user-emacs-directory . "/home/sampath/projects/eemacs/spacemacs")))
+ ("prelude" . ((user-emacs-directory . "/home/sampath/projects/eemacs/prelude")))
  ("doom" . ((user-emacs-directory . "~/config/doomemacs")
             (env . (("DOOMDIR" . "~/doom-config")))))
  )
--- tmuxconfig.sh
+++ tmuxconfig.sh
@@ -1,63 +1,64 @@
-# put this in ~/.tmux.conf
+# put this in @~/.tmux.conf
+# ln -s ~/projects/settings/tmuxconfig.sh ~/.tmux.conf
 # source-file ~/settings/tmuxconfig.sh
 # set -g default-shell /usr/bin/zsh
 set -g default-shell $SHELL
 set -g prefix C-a
 
 # # bind 'C-x C-x' to type 'C-x'
-bind C-a send-prefix
-set -g mouse on
-set -g default-terminal "screen-256color"
-set -g terminal-overrides 'xterm*:smcup@:rmcup@'
+# bind C-a send-prefix
+# set -g mouse on
+# set -g default-terminal "screen-256color"
+# set -g terminal-overrides 'xterm*:smcup@:rmcup@'
 # # Use emacs keybindings for tmux commandline input.
 # # (Note: if in vi mode, to get command mode you need to hit ESC twice.)
-set -g status-keys emacs
+# set -g status-keys emacs
 # #cpy pste keys
-set-window-option -g mode-keys emacs
+# set-window-option -g mode-keys emacs
 # # Window management / navigation
 
 # # move between windows
-bind  C-b previous-window
-bind  C-f next-window
-bind  C-l last-window
+# bind  C-b previous-window
+# bind  C-f next-window
+# bind  C-l last-window
 
 # pane selection commands
-bind  p select-pane -U
-bind  n select-pane -D
-bind  b select-pane -L
-bind  f select-pane -R
+# bind  p select-pane -U
+# bind  n select-pane -D
+# bind  b select-pane -L
+# bind  f select-pane -R
 
-unbind [
-bind  C-w copy-mode
-set-option -g history-limit 2000000
-# bind -t vi-copy v begin-selection
-# bind -t vi-copy y copy-selection
-# bind -t vi-copy Escape cancel
-# #bind -t vi-copy 'v' begin-selection
-# #bind -t vi-copy 'y' copy-selection
-# #set -g mode-mouse on
-set -g @plugin 'tmux-plugins/tmux-yank'
-set -g @plugin 'tmux-plugins/tmux-copycat'
-set -g base-index 1
-setw -g pane-base-index 1
-# unbind-key -n C-r
-bind r source-file ~/.tmux.conf \; display "Reloaded!"
-bind  - split-window -v -c "#{pane_current_path}"
-bind  \\ split-window -h -c "#{pane_current_path}"
-bind  C-r send-keys -R \; clear-history
-# # List of plugins
-set -g @plugin 'tmux-plugins/tpm'
-set -g @plugin 'tmux-plugins/tmux-sensible'
-# close all tmux windows
-# # move x clipboard into tmux paste buffer
-# #bind C-p run "tmux set-buffer \"$(xclip -o)\"; tmux paste-buffer"
-# # move tmux copy buffer into x clipboard
-# #bind C-y run "tmux save-buffer - | xclip -i"
+# unbind [
+# bind  C-w copy-mode
+# set-option -g history-limit 2000000
+# # bind -t vi-copy v begin-selection
+# # bind -t vi-copy y copy-selection
+# # bind -t vi-copy Escape cancel
+# # #bind -t vi-copy 'v' begin-selection
+# # #bind -t vi-copy 'y' copy-selection
+# # #set -g mode-mouse on
+# set -g @plugin 'tmux-plugins/tmux-yank'
+# set -g @plugin 'tmux-plugins/tmux-copycat'
+# set -g base-index 1
+# setw -g pane-base-index 1
+# # unbind-key -n C-r
+# bind r source-file ~/.tmux.conf \; display "Reloaded!"
+# bind  - split-window -v -c "#{pane_current_path}"
+# bind  \\ split-window -h -c "#{pane_current_path}"
+# bind  C-r send-keys -R \; clear-history
+# # # List of plugins
+# set -g @plugin 'tmux-plugins/tpm'
+# set -g @plugin 'tmux-plugins/tmux-sensible'
+# # close all tmux windows
+# # # move x clipboard into tmux paste buffer
+# # #bind C-p run "tmux set-buffer \"$(xclip -o)\"; tmux paste-buffer"
+# # # move tmux copy buffer into x clipboard
+# # #bind C-y run "tmux save-buffer - | xclip -i"
 
-bind-key -r < swap-window -t -
-bind-key -r > swap-window -t +
+# bind-key -r < swap-window -t -
+# bind-key -r > swap-window -t +
 
 
-run '~/.tmux/plugins/tpm/tpm'
+# run '~/.tmux/plugins/tpm/tpm'
 
-bind X kill-server
+# bind X kill-server
