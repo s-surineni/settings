e099938054340889598b765f4ab9d69e8389bdd2
Sampath Surineni <sampath.surineni@gmail.com>
2024-01-07 20:51:31 +0530

    removed zsh config

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,18 +1,3 @@
-if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]
-then
-    echo "auto suggestions installed"
-else
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-fi
-
-# after this do ctrl - a + I to install packages
-if [ -d ~/.tmux/plugins/tpm ]
-then
-    echo "tmux package manager installed"
-else
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
 plugins=(
     git
     zsh-autosuggestions
