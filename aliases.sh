e46d71dc6b88d94f805f50ffddc1e2854614707c
ssurineni <ssurineni@salesforce.com>
2024-06-25 23:15:59 +0530

    added mroe aliases

--- aliases.sh
+++ aliases.sh
@@ -3,14 +3,14 @@ alias act="git commit -am "
 alias ct="git commit -m "
 alias cot="git checkout "
 alias stt="git status"
-
+alias pullhead='git pull origin $(git rev-parse --abbrev-ref HEAD)'
 alias x="exit"
+alias gitcl="git checkout -- ."
 
 
 alias emax="emacsclient -n"                      # used to be "emacs -nw"
 alias semac="sudo emacsclient -t"                # used to be "sudo emacs -nw"
 alias emacsc="emacsclient -c -a emacs"           # new - opens the GUI with alternate non-daemon
-alias sttu="git status -uno"        
+alias sttu="git status -uno"
 alias emaxs="SUDO_EDITOR=\"emacsclient -n\" sudo -e"
 alias tmux="tmux -u"
-
