3fb1b6cddce6f24f5dbc1039075c77449ece86b7
sampath surineni <sampath.surineni@gmail.com>
2025-01-24 23:01:06 +0530

    updated windows settings

--- window_settings.ps1
+++ window_settings.ps1
@@ -1,2 +1,16 @@
 function Get-GitStatus { git status } 
 Set-Alias -Name stt -Value Get-GitStatus
+
+
+function gacp {
+    param (
+        [string]$message
+    )
+
+    git add .
+    git commit -m $message
+    git push
+}
+
+# Export the function to be available in the session
+# Export-ModuleMember -Function gacp
